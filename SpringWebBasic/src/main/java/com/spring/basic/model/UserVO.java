89d3cc890cf92c4083065a7e902d86b88c3ab917
yangwoo <kyy77321@naver.com>
2023-09-18 17:28:49 +0900

    2023-09-18수업

---
+++ SpringWebBasic/src/main/java/com/spring/basic/model/UserVO.java
@@ -0,0 +1,26 @@
+package com.spring.basic.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserVO {
+
+	// join에 name과 똑같은 이름을 사용해야함
+
+	private String userId;
+	private String userPw;
+	private String userName;
+	private List<String> hobby;
+	
+}
